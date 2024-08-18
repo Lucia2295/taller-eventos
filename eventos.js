649010a7a775000822ef8da3e9fdcd00d8d0b03b
lucia2295 <lucia.barreto95@gmail.com>
2024-08-17 21:13:36 -0300

    Evento al div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    let eventodiv = document.getElementById("eldiv");
+    eventodiv.addEventListener("click", () => (
+     alert("Hola! Soy el div.")
+   ));})
