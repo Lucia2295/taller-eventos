621aadde16214d79098c6fb97668edafe0786b79
lucia2295 <lucia.barreto95@gmail.com>
2024-08-18 00:03:52 -0300

    Solucion click en Saludar

--- eventos.js
+++ eventos.js
@@ -1,6 +1,19 @@
+
 document.addEventListener('DOMContentLoaded',function(){
 
-    let eventodiv = document.getElementById("eldiv");
-    eventodiv.addEventListener("click", () => (
-     alert("Hola! Soy el div.")
-   ));})
+// Seleccionamos los elementos
+let botonsaludar = document.getElementById('elboton');
+let divdelboton = document.getElementById('eldiv');
+
+
+// Agregamos un evento 'click' al botón
+botonsaludar.addEventListener('click', (event) => {
+    event.stopPropagation(); // Detener la propagación del evento
+    alert('Hola!');
+});
+
+// Agregamos un evento 'click' al div del botón
+divdelboton.addEventListener('click', () => {
+    alert('Hola! Soy el div.');
+    
+});})
